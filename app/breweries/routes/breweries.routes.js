f38c4b06eeac73579e185c727cab409c5f3ad24e
Rafael Barzotto <rafabarzotto@hotmail.com>
2016-02-13 10:34:30 -0200

    Added methods

--- app/breweries/routes/breweries.routes.js
+++ app/breweries/routes/breweries.routes.js
@@ -3,6 +3,15 @@
 module.exports = function(api) {
 	var breweries = require('../controllers/breweries.controller');
 
-	api.route('/breweries').get(breweries.findAll);
+	api.route('/breweries')
+		.get(breweries.findAll)
+		.post(breweries.create);
+
+	api.route('/breweries/:breweryId')
+		.get(breweries.find)
+		.put(breweries.update)
+		.delete(breweries.delete);
+
+	api.param('breweryId', breweries.breweryById);		
 
 }
